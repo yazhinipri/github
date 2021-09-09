bfb232bac3b1dea0c13e9d84bf007d36c376a795
yazhinipri <90320829+yazhinipri@users.noreply.github.com>
2021-09-09 10:45:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,20 @@
-provider "google" {,
-
-project = "potent-galaxy-321008"
-
+provider "google" {
+  project = "mesmerizing-bee-324807"
 }
-
 resource "google_service_account" "default" {
-
-account_id = "potent-galaxy-321008"
-
-display_name = "test-terraform2"
-
+  account_id = "mesmerizing-bee-324807"
+  display_name = "test-terraform2" 
 }
-
 resource "google_container_cluster" "primary" {
-
-name = "my-gke-cluster2"
-
-location = "us-central1"
-
-remove_default_node_pool = true
-
-initial_node_count = 1
-
+  name = "my-gke-cluster2"
+  location = "us-central1"
+  remove_default_node_pool = true
+  initial_node_count = 1
 }
-
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-
-name = "my-node-pool"
-
-location = "us-central1"
-
-cluster = google_container_cluster.primary.name
-
-node_count = 1
-
+  name = "my-node-pool"
+  location = "us-central1"
+  cluster = google_container_cluster.primary.name
+  node_count = 1
 }
+    
